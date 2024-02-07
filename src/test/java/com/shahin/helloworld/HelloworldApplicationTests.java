70b1dcd7bcbde4e57acd46906d67dede4d59b42f
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-02-07 11:39:39 +0100

    create the tests

--- src/test/java/com/shahin/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/shahin/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,24 @@
 package com.shahin.helloworld;
 
+import com.shahin.helloworld.service.BusinessService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloworldApplicationTests {
+    @Autowired
+    private BusinessService bs;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void should_say_hello_world() {
+
+        assertThat(bs.getHelloWorld().getValue()).isEqualTo("Hello World!");
+    }
 }
